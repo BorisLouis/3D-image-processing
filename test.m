5a12a657943e1fbbf3f541e4baede53554a1460f
Steven Huysecom <steven.huysecom@hotmail.com>
2025-02-27 17:12:37 +0100

    backcalculation ang velocity

--- test.m
+++ test.m
@@ -1,1 +1,10 @@
-MeanTotInt = [];
+for i = 1:size(CommonTraces, 1)
+    Trace1 = CommonTraces.Int1{i,1};
+    Trace2 = CommonTraces.Int2{i,1};
+
+    TotTrace = Trace1 + Trace2;
+    MeanTot = median(TotTrace);
+    
+    CommonTraces.TotIntTrace{i,1} = TotTrace;
+    CommonTraces.MeanTotInt{i,1} = MeanTot;
+end
