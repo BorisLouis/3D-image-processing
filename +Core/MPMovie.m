b052986e494b33fbfd1f747401708ea937f96c3e
StevenHuysecom <steven.huysecom@hotmail.com>
2025-04-08 07:59:55 +0200

    minor changes

--- +Core/MPMovie.m
+++ +Core/MPMovie.m
@@ -302,7 +302,6 @@ classdef MPMovie < Core.Movie
                                 CDiff = [CMa(1) - CMb(1), CMa(2) + CMb(2)];
                             end
                             Transformation = obj.SRCal{2, 1}.Transformations{i, 1};
-                            % Transformation.Translation = [Transformation.Translation(1) - CDiff(1)*Transformation.Scale + 4, Transformation.Translation(2) - CDiff(2)*Transformation.Scale + 10];
                             % Transformation.Translation = [Transformation.Translation(1) - CDiff(1), Transformation.Translation(2)+ CDiff(2)];
                             Transformation.Translation = [Transformation.Translation(1), Transformation.Translation(2)];
                             Transformation.R = [cosd(Transformation.RotationAngle) -sind(Transformation.RotationAngle);
