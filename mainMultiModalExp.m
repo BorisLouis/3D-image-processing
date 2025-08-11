f13f820b68a99298381628921d09b6a22b93c660
Steven Huysecom <steven.huysecom@hotmail.com>
2025-08-11 13:51:55 +0200

    last changes

--- mainMultiModalExp.m
+++ mainMultiModalExp.m
@@ -6,13 +6,9 @@ path2ZCal = [];
 path2SRCal = 'S:\Rotational Tracking\20250708_AuBPs_184x92_glycerol\2DCal';
 
 %file info
-<<<<<<< HEAD
-file.path  = 'D:\Multimodal tracking\20250724\alldata';
-path2Cal = 'D:\Multimodal tracking\20250724\2DCal';
-=======
+
 file.path  = 'S:\Rotational Tracking\20250708_AuBPs_184x92_glycerol\alldata';
 path2Cal = 'S:\Rotational Tracking\20250708_AuBPs_184x92_glycerol\2DCal';
->>>>>>> 789a2706a2455919fa94798e7f7a1f6f4c444a9a
 [info, info1, info2, file] = UserInput.infoGUI(file);
 
 %% create experiments
