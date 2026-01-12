0ff435c08915d3963ff4830b76edecfa57ef7bcb
Steven Huysecom <steven.huysecom@hotmail.com>
2026-01-12 16:27:09 +0100

    fix stepsizeanalysis pt 1

--- mainCalcMSD_microrheology.m
+++ mainCalcMSD_microrheology.m
@@ -5,7 +5,7 @@ close all;
 [raw.FilePath, info.Experiment, info.FilenameRaw, info.Dimension, info.expTime, info.Temp, info.Radius1, info.Radius2, info.DiffFit, info.MinSize, info.Ext, info.ParticleType, info.path2RotCal, info.CutTraces, info.ExpModel, info.StepsizeAnalysis] = UserInput.CalcMSDinfoGUI;
 
 MainFolder = 'D:\Polymer Dynamics';
-SubFolders = {'PAA_1x_bAA', 'PAA_2x_bAA', 'PAA_3x_bAA', 'PAA_4x_bAA', 'PAA_2x_AA', 'PAA_3x_AA'};
+SubFolders = {'PAA_3x_bAA', 'PAA_4x_bAA', 'PAA_2x_AA', 'PAA_3x_AA'};
 SubSubFolders = {'time12', 'time3', 'time6'};
 ExpTimes = [0.05, 0.01, 0.01];
 CutTraces = [10, 10, 10];
